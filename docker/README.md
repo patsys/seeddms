1bfcd73a8a1dcd2f2c9326d701bd487c1068c6ce
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2020-12-30 21:57:51 +0100

    add change initial password

--- docker/README.md
+++ docker/README.md
@@ -1,5 +1,5 @@
 Environment SET_configuration_ will used to change seeddms config as exapmel SET_configuration_server_database__dbuser change the dbuser and DELETE_configuration_ will remove a setting
-
+Environment ADMIN_INITIAL_PASSWORD use to set inintialspassword
 As setting is under /var/templates/seeddms/conf/settings.xml 
 Data saved under /var/www/seeddms/store
 
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -45,10 +45,33 @@ for file in /var/seeeddms/hooks/*; do
   fi
 done
 
-mkdir -p /var/www/seeddms/store/{staging lucene content backup drop}
+mkdir -p /var/www/seeddms/store/{staging,lucene,content,backup,drop}
 if [ ! -f /var/www/seeddms/store/content.db ]; then
   cp /var/www/seeddms/data/content.db /var/www/seeddms/store/content.db
 fi
 chown -R www-data:www-data /var/www/seeddms
-
+touch /var/www/seeddms/conf/ENABLE_INSTALL_TOOL
+apachectl start
+sleep 5
+params="$(xmlstarlet sel -T -t -m '//form//input[@name and @value]' -v "concat(\" -F '\",@name,\"=\",@value,\"'\")" <<<"$(curl http://127.0.0.1/install/install.php | xmlstarlet fo -H -R )")"
+eval $(echo curl -k -X POST "$params" -F "createDatabase=on" http://127.0.0.1/install/install.php)
+sleep 5
+eval $(echo curl -k -X POST "$params"  http://127.0.0.1/install/install.php)
+rm /var/www/seeddms/conf/ENABLE_INSTALL_TOOL
+if [ -n "$ADMIN_INITIAL_PASSWORD" ]; then
+  tmp="$(mktemp /tmp/cookie.XXXXXX)"
+  if curl -f -c $tmp -k -X POST "http://127.0.0.1/restapi/index.php/login" -H  "accept: application/json" -H  "Content-Type: application/x-www-form-urlencoded" -d "user=admin" -d "pass=admin"; then
+     if curl -f -b $tmp -k -X PUT "http://127.0.0.1/restapi/index.php/users/1/password" -H "Content-Type: application/x-www-form-urlencoded" -H  "accept: application/json" --data-urlencode "password=$(echo -n "$ADMIN_INITIAL_PASSWORD" | md5sum | awk '{print $1}')"; then
+       echo "password change"
+       rm $tmp
+     else
+       echo "password change failed" >&2
+       rm $tmp
+       exit 1
+     fi
+  fi
+unset ADMIN_INITIAL_PASSWORD
+fi
+apachectl stop
+sleep 2
 apachectl -D FOREGROUND
